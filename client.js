ad362183bc41e1b3fca4aadf8689da970e7db814
Mark Sherman <msherman@cs.uml.edu>
2015-06-25 16:53:20 -0400

    added closure lib, with hello world

--- client.js
+++ client.js
@@ -1,1 +1,7 @@
-alert("Javascript is working!");
+goog.require('goog.dom');
+
+function sayHi() {
+  var newHeader = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
+    'Hello world!');
+  goog.dom.appendChild(document.body, newHeader);
+}
