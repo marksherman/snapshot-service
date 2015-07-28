e7d66968494d4bed588fe767b662d4765da0fefd
Mark Sherman <msherman@cs.uml.edu>
2015-07-28 16:39:20 -0400

    name-existence checks work, working on generating unique names

--- server/dbtest.js
+++ server/dbtest.js
@@ -2,16 +2,18 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('usermap.sqlite3');
 var codename = require('codename')();
 
-function username_exists (username) {
+function some_name_exists (colname, username) {
 	return new Promise(
 		function(resolve, reject) {
 			// no need to serialize, db action is read-only
-			db.get("SELECT EXISTS(SELECT 1 from usermap WHERE username = ? LIMIT 1)", username,
+			db.get("SELECT EXISTS(SELECT 1 from usermap WHERE " + colname + " = ? LIMIT 1)", username,
 				function(err, row){
-					if( row["EXISTS(SELECT 1 from usermap WHERE username = ? LIMIT 1)"] == 1 ){
+					if( row["EXISTS(SELECT 1 from usermap WHERE " + colname + " = ? LIMIT 1)"] === 1 ){
 						resolve(true);
-					} else {
+					} else if( row["EXISTS(SELECT 1 from usermap WHERE " + colname + " = ? LIMIT 1)"] === 0 ){
 						resolve(false);
+					} else {
+						reject(new Error("database did not return an expected value in username_exists"));
 					}
 				}
 			);
@@ -19,17 +21,38 @@ function username_exists (username) {
 	);
 }
 
+function username_exists (username) {
+	return some_name_exists("username", username);
+}
+
+function codename_exists (username) {
+	return some_name_exists("codename", username);
+}
+
+
 function generate_random_name(){
 	var tempname = codename.generate(['random'],['adjectives','animals']);
 	return tempname[0]+tempname[1];
 }
 
 function generate_unique_name(){
-	var name = generate_random_name();
-
-
-
-
+	return new Promise(
+		function(resolve, reject){
+			// get a new name
+			var name = generate_random_name();
+			// check to make sure it is unique
+			var existp = codename_exists(name);
+			existp.then(function(value){
+				if( value === false ){
+					// name not found. it's unique! return it.
+					resolve(name);
+				} else {
+					// name was found or something else went wrong. not unique.
+					reject("bad name - please run generate_unique_name again");
+				}
+			});
+		}
+	);
 }
 
 function get_code_name (username) {
@@ -39,16 +62,16 @@ function get_code_name (username) {
 			var p = username_exists(username);
 			p.then(
 				function(value) {
-					if (value == 1){
+					if (value === true){
 						// username exists, just fetch the already-existing codename
 						db.get("SELECT codename FROM usermap WHERE username = ?", username,
 							function(err, row){
-								console.log("in promise got codename: " + row.codename);
 								resolve(row.codename);
 							});
-					} else {
+					} else if (value === false){
 						//TODO
 						// username does not exist, insert a new codename
+						resolve("no name found!");
 					}
 				}
 			);
@@ -77,11 +100,14 @@ var test_data = function () {
 
 
 
-console.log("about to run it all");
-var namep = get_code_name("Calliope");
-console.log("name promise returned " + namep);
-namep.then(function(value){
-	console.log("got name value: " + value);
+console.log("**********************************");
+var namepgood = get_code_name("Calliope");
+var namepbad = get_code_name("CalliopeZZZZ");
+namepgood.then(function(value){
+	console.log("good name value: " + value);
+});
+namepbad.then(function(value){
+	console.log("bad name value: " + value);
 });
 
 //  db.each("SELECT id, username, codename FROM usermap", function(err, row) {
