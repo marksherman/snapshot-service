84d5d547da168a665028ba12c6283b35a4471a0b
Mark Sherman <msherman@cs.uml.edu>
2015-08-11 13:15:24 -0400

    Cleaned up. Broke log out of exports list, can be changed easier now.

--- server/modules/file.js
+++ server/modules/file.js
@@ -1,38 +1,40 @@
 module.exports = {
-  log: function (data) {
-    console.log("\n\n--------------------------------------\n");
-    console.log("Snapshot recieved at " + new Date());
-    console.log(sanitizeFilename(data));
-
-    return "0";
-  }
+  log: consolelog
 };
 
+function consolelog (data) {
+  console.log("\n\n--------------------------------------\n");
+  console.log("Snapshot recieved at " + new Date());
+  console.log(sanitizeFilename(data));
+
+  return "0";
+}
+
 /**
-     * Sanitize a file name by replacing ".." with "DOTDOT", backslash with
-     * slash, multiple adjacent slashes with a single slash, and any remaining
-     * slash with "SLASH".
-     *
-     * @author Derrell Lipman
-     * @param name {String}
-     *   The file name to be sanitized
-     *
-     * @return {String}
-     *   The sanitized file name
-     */
-     sanitizeFilename = function(name)
-    {
-      // Remove dangerous ..
-      name = name.replace(/\.\./g, "DOTDOT");
+* Sanitize a file name by replacing ".." with "DOTDOT", backslash with
+* slash, multiple adjacent slashes with a single slash, and any remaining
+* slash with "SLASH".
+*
+* @author Derrell Lipman
+* @param name {String}
+*   The file name to be sanitized
+*
+* @return {String}
+*   The sanitized file name
+*/
+sanitizeFilename = function(name)
+{
+  // Remove dangerous ..
+  name = name.replace(/\.\./g, "DOTDOT");
 
-      // Replace backslashes with forward slashes
-      name = name.replace(/\\/g, "/");
+  // Replace backslashes with forward slashes
+  name = name.replace(/\\/g, "/");
 
-      // Strip any double slashes; replace with single slashes
-      name = name.replace(/\/+/g, "/");
+  // Strip any double slashes; replace with single slashes
+  name = name.replace(/\/+/g, "/");
 
-      // Replace dangerous slashes
-      name = name.replace(/\//g, "SLASH");
+  // Replace dangerous slashes
+  name = name.replace(/\//g, "SLASH");
 
-      return name;
-    }
+  return name;
+};
