75e33274f20035c2d28e4fa630040729932b18f1
Mark Sherman <msherman@cs.uml.edu>
2015-09-02 11:35:20 -0400

    Cleaned test client

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 goog.require('goog.net.XhrIo');
 
-
+var count = 101;
 /**
  * Retrieve Json data using XhrIo's static send() method.
  *
@@ -11,24 +11,15 @@ function getData(dataUrl) {
 		{	"jsonrpc": "2.0",
 			"method": "math.add",
 			"params": [2, 3],
-			"id": 97 }
+			"id": count }
 		);
-  log('Sending simple request for ['+ dataUrl + ']');
-  goog.net.XhrIo.send(dataUrl, function(e) {
-      var xhr = e.target;
-      var obj = xhr.getResponseJson();
-      log('Result: ' + obj.result);
-      console.log(obj);
+  console.log('Sending simple request for [' + dataUrl + ']' + ' # ' + count);
+
+  goog.net.XhrIo.send(dataUrl, function (e) {
+      var xhr = e.target,
+					obj = xhr.getResponseJson();
+      console.log('Result: ' + obj.result);
+			console.log(obj);
   },
   "POST", content);
-}
-
-/**
- * Basic logging to an element called "log".
- *
- * @param {string} msg Message to display on page
- */
-function log(msg) {
-  document.getElementById('log').appendChild(document.createTextNode(msg));
-  document.getElementById('log').appendChild(document.createElement('br'));
 }
