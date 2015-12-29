13d258552cceefd06be5aa31601739a9c153e40d
Mark Sherman <msherman@cs.uml.edu>
2015-12-29 14:57:53 -0500

    renamed rpc modules dir to rpc_modules

--- server/snapshot-service.js
+++ server/snapshot-service.js
@@ -10,14 +10,13 @@
 var rpc = require('jrpc2');
 var express = require('express');
 var cors = require('cors');
-var userdb = require('./userdb.js')();
 var app = express();
 var rpcServer = new rpc.Server();
 var Log = require('./loglevel.js')();
 
 app.use(cors());
 
-rpcServer.loadModules(__dirname + '/modules/', function () {
+rpcServer.loadModules(__dirname + '/rpc_modules/', function () {
   app.post('/', rpc.middleware(rpcServer));
   app.listen(8000);
 });
