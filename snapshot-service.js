4377d4da28a0b50fe8ca66e9853d2cdd21effeaa
Mark Sherman <msherman@cs.uml.edu>
2018-02-15 13:35:37 -0500

    Revert "use port 80 when in production"
    This reverts commit 53b05ef042444621ec3199ab9c6b18e715997ff9.

--- snapshot-service.js
+++ snapshot-service.js
@@ -7,23 +7,18 @@
  *   GPL-3.0 : https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 
-const rpc = require('jrpc2');
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const rpcServer = new rpc.Server();
-const Log = require('./loglevel.js')();
+var rpc = require('jrpc2');
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var rpcServer = new rpc.Server();
+var Log = require('./loglevel.js')();
 
 app.use(cors());
 
-let port = 8000;
-if (process.env.NODE_ENV === 'production'){
-    port = 80;
-}
-
 rpcServer.loadModules(__dirname + '/rpc_modules/', function () {
   app.post('/v1.0', rpc.middleware(rpcServer));
-  app.listen(port, function() {
+  app.listen(8000, function() {
     Log.log("server running from " + __dirname);
   });
 });
