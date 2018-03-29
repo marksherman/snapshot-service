6c793c5a5a40b319f9d8b272f46baf3ebc402020
Mark Sherman <msherman@cs.uml.edu>
2018-03-29 14:04:45 -0400

    Fixed indenting

--- client/client.js
+++ client/client.js
@@ -11,18 +11,18 @@
 var count = 101;
 
 var testdata =
-{
-	"userName":"test@client.html",
-	"projectName":"TestClientProject",
-	"projectId":"9999999999999999",
-	"screenName":"Screen1",
-	"sessionId":"2be55c43-d3af-4806-8694-ccc6ec93414b",
-	"yaversion":140,
-	"languageVersion":19,
-	"eventType":"pollYail",
-	"blocks":"<xml xmlns=\"http://www.w3.org/1999/xhtml\">\n  <block type=\"text_join\" id=\"1\" inline=\"false\" x=\"259\" y=\"346\">\n    <mutation items=\"2\"></mutation>\n    <value name=\"ADD0\">\n      <block type=\"color_red\" id=\"21\">\n        <field name=\"COLOR\">#ff0000</field>\n      </block>\n    </value>\n  </block>\n  <yacodeblocks ya-version=\"140\" language-version=\"19\"></yacodeblocks>\n</xml>",
-	"form":"{\"YaVersion\":\"140\",\"Source\":\"Form\",\"Properties\":{\"$Name\":\"Screen1\",\"$Type\":\"Form\",\"$Version\":\"18\",\"AppName\":\"asdf\",\"Title\":\"Screen1\",\"Uuid\":\"0\"}}"
-};
+    {
+      "userName":"test@client.html",
+      "projectName":"TestClientProject",
+      "projectId":"9999999999999999",
+      "screenName":"Screen1",
+      "sessionId":"2be55c43-d3af-4806-8694-ccc6ec93414b",
+      "yaversion":140,
+      "languageVersion":19,
+      "eventType":"pollYail",
+      "blocks":"<xml xmlns=\"http://www.w3.org/1999/xhtml\">\n  <block type=\"text_join\" id=\"1\" inline=\"false\" x=\"259\" y=\"346\">\n    <mutation items=\"2\"></mutation>\n    <value name=\"ADD0\">\n      <block type=\"color_red\" id=\"21\">\n        <field name=\"COLOR\">#ff0000</field>\n      </block>\n    </value>\n  </block>\n  <yacodeblocks ya-version=\"140\" language-version=\"19\"></yacodeblocks>\n</xml>",
+      "form":"{\"YaVersion\":\"140\",\"Source\":\"Form\",\"Properties\":{\"$Name\":\"Screen1\",\"$Type\":\"Form\",\"$Version\":\"18\",\"AppName\":\"asdf\",\"Title\":\"Screen1\",\"Uuid\":\"0\"}}"
+    };
 
 goog.require('goog.dom');
 
@@ -48,7 +48,7 @@ var idno = 0;
 // "Complete" is called before success or error
 var ss_xhrComplete = function()
 {
-	// Put in here anything to run regardless of success/error
+  // Put in here anything to run regardless of success/error
   console.log('%%%% getData COMPLETE');
 
 };
@@ -58,15 +58,15 @@ var ss_xhrComplete = function()
 var ss_xhrSuccess = function() {
   var obj = this.getResponseJson();
   ss_xhrPool.releaseObject(this);
-	console.log('%%%% getData Success - result: ' + obj.result);
-	//console.log(obj);
+  console.log('%%%% getData Success - result: ' + obj.result);
+  //console.log(obj);
 };
 
 // Listen for Error-result RPC calls
 // Error event is fired AFTER complete event.
 var ss_xhrError = function() {
   ss_xhrPool.releaseObject(this);
-	console.log('%%%% getData resulted in error.');
+  console.log('%%%% getData resulted in error.');
 };
 
 /**
@@ -76,76 +76,76 @@ var ss_xhrError = function() {
  * @param {string} snapshot data to send.
  */
 blockly_send = function(projectData) {
-	var pd = _.assign({}, projectData,
-		{
-			"sendDate":new Date().toJSON()
-		}
-	);
+  var pd = _.assign({}, projectData,
+      {
+        "sendDate":new Date().toJSON()
+      }
+  );
 
-	var data = [
-		JSON.stringify(pd)
-	];
+  var data = [
+    JSON.stringify(pd)
+  ];
 
-	var content = goog.json.serialize(
-	{	"jsonrpc": "2.0",
-		"method": "file.log",
-		"params": data,
-		"id": ++idno }    // dirty, i know -Mark
-	);
+  var content = goog.json.serialize(
+      {	"jsonrpc": "2.0",
+        "method": "file.log",
+        "params": data,
+        "id": ++idno }    // dirty, i know -Mark
+  );
 
-	console.log("\n\n------ Snapshot! ------ " + new Date() + "\n");
-	//console.log("Data:\n");
-	//console.log(content);
+  console.log("\n\n------ Snapshot! ------ " + new Date() + "\n");
+  //console.log("Data:\n");
+  //console.log(content);
 
-	goog.net.XhrIo.send(dataUrl, ss_xhrComplete, "POST", content);
+  goog.net.XhrIo.send(dataUrl, ss_xhrComplete, "POST", content);
 
 };
 
 /*******************************/
 
 var send = function(){
-	blockly_send(_.assign({}, testdata,
-		{
-			"eventType":"Testbutton click"
-		}
-	));
+  blockly_send(_.assign({}, testdata,
+      {
+        "eventType":"Testbutton click"
+      }
+  ));
 };
 
 var jiggle = function(){
-	var jiggledBlocks =
-		"<xml xmlns=\"http://www.w3.org/1999/xhtml\">\n  <block type=\"text_join\" id=\"1\" inline=\"false\" x=\"" +
-		_.random(0,999).toString() +
-		"\" y=\"" +
-		_.random(0,999).toString() +
-		"\">\n    <mutation items=\"2\"></mutation>\n    <value name=\"ADD0\">\n      <block type=\"color_red\" id=\"21\">\n        <field name=\"COLOR\">#ff0000</field>\n      </block>\n    </value>\n  </block>\n  <yacodeblocks ya-version=\"140\" language-version=\"19\"></yacodeblocks>\n</xml>";
+  var jiggledBlocks =
+      "<xml xmlns=\"http://www.w3.org/1999/xhtml\">\n  <block type=\"text_join\" id=\"1\" inline=\"false\" x=\"" +
+      _.random(0,999).toString() +
+      "\" y=\"" +
+      _.random(0,999).toString() +
+      "\">\n    <mutation items=\"2\"></mutation>\n    <value name=\"ADD0\">\n      <block type=\"color_red\" id=\"21\">\n        <field name=\"COLOR\">#ff0000</field>\n      </block>\n    </value>\n  </block>\n  <yacodeblocks ya-version=\"140\" language-version=\"19\"></yacodeblocks>\n</xml>";
 
-	var jiggledData = _.assign({}, testdata,
-		{
-			"blocks":jiggledBlocks,
-			"eventType":"Test jiggle button click"
-		});
+  var jiggledData = _.assign({}, testdata,
+      {
+        "blocks":jiggledBlocks,
+        "eventType":"Test jiggle button click"
+      });
 
-	blockly_send(jiggledData);
+  blockly_send(jiggledData);
 };
 
 var hello = function(){
 
-    var content = goog.json.serialize(
-        {	"jsonrpc": "2.0",
-            "method": "sayHello",
-            "params": "{}",
-            "id": ++idno }    // dirty, i know -Mark
-    );
+  var content = goog.json.serialize(
+      {	"jsonrpc": "2.0",
+        "method": "sayHello",
+        "params": "{}",
+        "id": ++idno }    // dirty, i know -Mark
+  );
 
-    console.log("\n\n------ Saying Hello to server ------ " + new Date() + "\n");
+  console.log("\n\n------ Saying Hello to server ------ " + new Date() + "\n");
 
-    goog.net.XhrIo.send(dataUrl, function(e) {
-        const obj = e.target.getResponseJson();
-        console.log('%%%% sayHello COMPLETE');
-        if (obj["result"] === "Hello!") {
-            console.log('%%%% sayHello Server said hello at ' + dataUrl);
-        }
-    }, "POST", content);
+  goog.net.XhrIo.send(dataUrl, function(e) {
+    const obj = e.target.getResponseJson();
+    console.log('%%%% sayHello COMPLETE');
+    if (obj["result"] === "Hello!") {
+      console.log('%%%% sayHello Server said hello at ' + dataUrl);
+    }
+  }, "POST", content);
 };
 
 /*******************************/
